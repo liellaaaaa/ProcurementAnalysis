af6e199a66d6b914bd702f43c7688ce24ee224ed
liellaaaaa <1337706441@qq.com>
2026-05-19 19:21:28 +0800

    修改配置

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
         target: 'http://localhost:8000',
         changeOrigin: true
       }
-    }
+    },
+    allowedHosts: ['fritter-bagpipe-credible.ngrok-free.dev']
   }
 })
